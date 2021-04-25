9862a9d593157cbf8ef3976920441a7b0343587b
s7thiago <thyagosousasilva@gmail.com>
2021-04-25 12:59:03 -0300

    Day 4: - Improved questions consuming from inner database - added an animation when a question os tapped on home screen - Some structural improvements

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,10 +1,13 @@
 import 'package:devquiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:devquiz/challenge/widgets/question_indicator/question_indicator.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
-import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
+import 'package:devquiz/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
+  final List<QuestionModel> questions;
+
+  const ChallengePage({Key? key, required this.questions}) : super(key: key);
   @override
   _ChallengePageState createState() => _ChallengePageState();
 }
@@ -14,16 +17,24 @@ class _ChallengePageState extends State<ChallengePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
+        preferredSize: Size.fromHeight(100),
         child: SafeArea(
           top: true,
-          child: QuestionIndicatorWidget(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BackButton(),
+              QuestionIndicatorWidget(),
+            ],
+          ),
         ),
       ),
-      body: QuizWidget(title: 'O que o Flutter faz em sua totalidade? '),
+      body: QuizWidget(
+        question: widget.questions[0],
+      ),
       bottomNavigationBar: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,44 +1,32 @@
+import 'dart:math';
+
 import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class QuizWidget extends StatelessWidget {
-  final String title;
+  final QuestionModel question;
 
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+  const QuizWidget({Key? key, required this.question}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          SizedBox(height: 64),
           Text(
-            title,
+            question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(height: 24),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente',
-            isSelected: true,
-            isRight: true,
-          ),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente',
-            isSelected: false,
-            isRight: false,
-          ),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente',
-            isSelected: false,
-          ),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente',
-            isSelected: true,
-          ),
+          ...question.answers
+              .map((e) => AnswerWidget(
+                    title: e.title,
+                    isRight: e.isRight,
+                  ))
+              .toList()
         ],
       ),
     );
