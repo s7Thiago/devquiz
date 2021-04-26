4b833a9f9ff1a93586b7039efd1253c385d63059
s7thiago <thyagosousasilva@gmail.com>
2021-04-26 18:23:17 -0300

    Day 5 - Implementing share button tap action

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -2,6 +2,7 @@ import 'package:devquiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:devquiz/core/app_images.dart';
 import 'package:devquiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ResultPage extends StatelessWidget {
   final String title;
@@ -60,7 +61,11 @@ class ResultPage extends StatelessWidget {
                           child: Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 68),
                         child: NextButtonWidget.purple(
-                            label: 'Compartilhar', onTap: () {}),
+                            label: 'Compartilhar',
+                            onTap: () {
+                              Share.share(
+                                  'check out my website https://s7thiago.github.io/#');
+                            }),
                       )),
                     ],
                   ),
