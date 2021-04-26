f96bcd246cbe54a5bbd54864eb55934dc22f7012
s7thiago <thyagosousasilva@gmail.com>
2021-04-26 18:51:09 -0300

    Day 5 - Adding animation to ChartWidget

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -3,7 +3,7 @@ import 'package:devquiz/core/app_text_styles.dart';
 import 'package:devquiz/models/user_model.dart';
 import 'package:flutter/material.dart';
 
-class ChartWidget extends StatelessWidget {
+class ChartWidget extends StatefulWidget {
   final UserModel user;
 
   const ChartWidget({
@@ -12,27 +12,63 @@ class ChartWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _ChartWidgetState createState() => _ChartWidgetState();
+}
+
+class _ChartWidgetState extends State<ChartWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  void initAnimation() {
+    // Inicializando controller da animação
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    _animation =
+        Tween<double>(begin: 0.0, end: widget.user.score / 100).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOutQuad,
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void initState() {
+    initAnimation();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       height: 80,
       width: 80,
-      child: Stack(
-        children: [
-          Center(
-            child: Container(
-              height: 80,
-              width: 80,
-              child: CircularProgressIndicator(
-                strokeWidth: 10,
-                value: (user.score / 100).toDouble(),
-                backgroundColor: AppColors.chartSecondary,
-                valueColor:
-                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, _) => Stack(
+          children: [
+            Center(
+              child: Container(
+                height: 80,
+                width: 80,
+                child: CircularProgressIndicator(
+                  strokeWidth: 10,
+                  value: _animation.value,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                ),
               ),
             ),
-          ),
-          Center(child: Text('${user.score}%', style: AppTextStyles.heading)),
-        ],
+            Center(
+                child: Text('${widget.user.score}%',
+                    style: AppTextStyles.heading)),
+          ],
+        ),
       ),
     );
   }
